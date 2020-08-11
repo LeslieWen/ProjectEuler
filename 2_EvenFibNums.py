d8bbb16175f6de24707c1ba5d3ed6011d4a00af7
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-08-11 15:39:15 -0400

    Update 2_EvenFibNums.py

--- 2_EvenFibNums.py
+++ 2_EvenFibNums.py
@@ -1,1 +1,16 @@
+#Starting at 1 and 2
+def fib(nthterm):
+	num1,num2=1,2
+	count=0
+	sum=0
+	while count<nthterm:
+		#print(num1)
+		if(num1%2==0):
+			sum=sum+num1
+		temp=num1+num2
+		num1=num2
+		num2=temp
+		count=count+1
+	print(sum)
 
+fib(10)
